9f39b2b67efd3d89a61b125d359d1f042fb0aa24
Corentin <135442084+CoThoison@users.noreply.github.com>
2025-12-04 16:43:10 -0500

    Fix typo in iptables rule for established connections

--- firewall/firewall-rules.sh
+++ firewall/firewall-rules.sh
@@ -21,7 +21,7 @@ iptables -A FORWARD -i eth1 -o eth2 -m mac --mac-source 02:42:0a:64:01:0a -d 10.
 iptables -A FORWARD -i eth2 -o eth1 -m mac --mac-source 02:42:0a:64:02:0a -d 10.100.1.10 -p tcp --sport 502 -j ACCEPT
 iptables -A FORWARD -i eth2 -o eth1 -m mac --mac-source 02:42:0a:64:02:0a -d 10.100.1.10 -p tcp --dport 502 -j ACCEPT
 
-iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEP
+iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 iptables -A FORWARD -j LOG --log-prefix "FW-DROP: " --log-level 4
 
